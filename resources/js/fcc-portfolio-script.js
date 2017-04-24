f7d2fcdd07102038643065dc4de569f7763f861c
Iulia Sargu <kanpeki@users.noreply.github.com>
2017-04-24 12:57:33 +0300

    Initial commit

---
+++ resources/js/fcc-portfolio-script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $("body").scrollspy({
+    target: ".navbar",
+    offset: 70
+  });
+});
+
+$('.navbar-collapse a').click(function (e) {
+  $('.navbar-collapse').collapse('toggle');
+  $('.navbar-toggle').toggleClass('open');
+});
+
+$('.navbar-toggle').click(function (e) {
+  $(this).toggleClass('open');
+});
+
+$('#contentContainer').click(function() {
+  if($('.navbar-toggle').hasClass('open')) {
+     $('.navbar-collapse').collapse('toggle');
+    $('.navbar-toggle').toggleClass('open');
+  }
+});
